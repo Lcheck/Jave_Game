eff1fb5adbcf8676569e24301f1436c1b345b41a
mk0329kor <mk0329kor@gmail.com>
2021-11-06 16:20:10 +0900

    ss

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 <<<<<<< HEAD
-System.out.println("asddcecd555555dasdsadasdasdsdddd");
+System.out.println("ssssss");
 =======
 System.out.println("asdasasZsdasdsdddd");
 >>>>>>> 7b7c252cb1e7f06bb2ba8675a020a25aa70e9094
